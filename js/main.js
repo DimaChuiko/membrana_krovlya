7e78bace9b504686e6d7c2969b11a339e98f37f6
dimachuiko <dimachuiko@gmail.com>
2019-08-31 19:03:50 +0300

    upgrade file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿// импортируем Popper
+//= 
+
+//////////импортируем jQ
+//= 
+////////////импортируем необходимые js-файлы Bootstrap 4
+//= bootstrap.js
+//= easyResponsiveTabs.js
+//= jquery-func.js
+//= lightbox-plus-jquery.min.js
+/////////////импортируем свои js-файлы
+//= my.js
